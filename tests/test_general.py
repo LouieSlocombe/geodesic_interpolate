f50e587122f8e1abf2bf317148fd483c1dd46e73
Louie Slocombe <louies@hotmail.co.uk>
2025-07-20 14:09:07 -0700

    test files clean up

---
+++ tests/test_general.py
@@ -0,0 +1,26 @@
+import geodesic_interpolate as gi
+
+
+def test_case_ch():
+    # H+CH4_CH3+H2.xyz
+    pass
+
+
+def test_case_diels_alder():
+    # DielsAlder.xyz
+    pass
+
+
+def test_case_trp_cage_unfold():
+    # TrpCage_unfold.xyz
+    pass
+
+
+def test_case_collagen():
+    # collagen.xyz
+    pass
+
+
+def test_case_calcium_binding():
+    # calcium_binding.xyz
+    pass
