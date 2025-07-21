4553517e800e6ca9cb5c1097249316e53ea75ebb
Louie Slocombe <louies@hotmail.co.uk>
2025-07-20 20:17:49 -0700

    wip simp #5

--- geodesic_interpolate/interpolation.py
+++ geodesic_interpolate/interpolation.py
@@ -9,7 +9,7 @@ from .geodesic import Geodesic
 logger = logging.getLogger(__name__)
 
 
-def _mid_point(atoms, geom1, geom2, tol=1e-2, nudge=0.01, threshold=4):
+def _mid_point(atoms, geom1, geom2, tol=1e-2, nudge=0.01, threshold=4.0):
     # Process the initial geometries, construct coordinate system and obtain average internals
     geom1, geom2 = np.array(geom1), np.array(geom2)
     add_pair = set()
@@ -21,21 +21,13 @@ def _mid_point(atoms, geom1, geom2, tol=1e-2, nudge=0.01, threshold=4):
     # contant, then rerun the minimization until the coordinate system is consistant with the
     # interpolated geometry
     while True:
-        rijlist, re = get_bond_list(geom_list, threshold=threshold + 1, enforce=add_pair)
+        rij_list, re = get_bond_list(geom_list, threshold=threshold + 1.0, enforce=add_pair)
         scaler = morse_scaler(alpha=0.7, re=re)
-        w1, _ = compute_wij(geom1, rijlist, scaler)
-        w2, _ = compute_wij(geom2, rijlist, scaler)
+        w1, _ = compute_wij(geom1, rij_list, scaler)
+        w2, _ = compute_wij(geom2, rij_list, scaler)
         w = (w1 + w2) / 2
         d_min, x_min = np.inf, None
         friction = 0.1 / np.sqrt(geom1.shape[0])
-
-        def target_func(X):
-            """Squared difference with reference w0"""
-            wx, dwdR = compute_wij(X, rijlist, scaler)
-            delta_w = wx - w
-            val, grad = 0.5 * np.dot(delta_w, delta_w), np.einsum('i,ij->j', delta_w, dwdR)
-            logger.info("val=%10.3f  ", val)
-            return val, grad
 
         # The inner loop performs minimization using either end-point as the starting guess.
         for coef in [0.02, 0.98]:
@@ -43,14 +35,14 @@ def _mid_point(atoms, geom1, geom2, tol=1e-2, nudge=0.01, threshold=4):
             x0 += nudge * np.random.random_sample(x0.shape)
             logger.debug('Starting least-squares minimization of bisection point at %7.2f.', coef)
             result = least_squares(
-                lambda x: np.concatenate([compute_wij(x, rijlist, scaler)[0] - w, (x - x0) * friction]), x0,
-                lambda x: np.vstack([compute_wij(x, rijlist, scaler)[1], np.identity(x.size) * friction]), ftol=tol,
+                lambda x: np.concatenate([compute_wij(x, rij_list, scaler)[0] - w, (x - x0) * friction]), x0,
+                lambda x: np.vstack([compute_wij(x, rij_list, scaler)[1], np.identity(x.size) * friction]), ftol=tol,
                 gtol=tol)
             x_mid = result['x'].reshape(-1, 3)
             # Take the interpolated geometry, construct new pair list and check for new contacts
             new_list = geom_list + [x_mid]
             new_rij, _ = get_bond_list(new_list, threshold=threshold, min_neighbors=0)
-            extras = set(new_rij) - set(rijlist)
+            extras = set(new_rij) - set(rij_list)
             if extras:
                 logger.info('  Screened pairs came into contact. Adding reference point.')
                 # Update pair list then go back to the minimization loop if new contacts are found
@@ -58,7 +50,11 @@ def _mid_point(atoms, geom1, geom2, tol=1e-2, nudge=0.01, threshold=4):
                 add_pair |= extras
                 break
             # Perform local geodesic optimization for the new image.
-            smoother = Geodesic(atoms, [geom1, x_mid, geom2], 0.7, threshold=threshold, log_level=logging.DEBUG,
+            smoother = Geodesic(atoms,
+                                [geom1, x_mid, geom2],
+                                scaler=0.7,
+                                threshold=threshold,
+                                log_level=logging.DEBUG,
                                 friction=1)
             smoother.compute_displacements()
             width = max([np.sqrt(np.mean((g - smoother.path[1]) ** 2)) for g in [geom1, geom2]])
@@ -66,30 +62,37 @@ def _mid_point(atoms, geom1, geom2, tol=1e-2, nudge=0.01, threshold=4):
             logger.debug('  Trial path length: %8.3f after %d iterations', dist, result['nfev'])
             if dist < d_min:
                 d_min, x_min = dist, x_mid
-        else:  # Both starting guesses finished without new atom pairs.  Minimization successful
+        else:
             break
     return x_min
 
 
-def redistribute(atoms, geoms, nimages, tol=1e-2):
+def redistribute(atoms, geoms, n_images, tol=1e-2):
     _, geoms = align_path(geoms)
     geoms = list(geoms)
-    # If there are too few images, add bisection points
-    while len(geoms) < nimages:
+
+    # Add bisection points if there are too few images
+    while len(geoms) < n_images:
         dists = [np.sqrt(np.mean((g1 - g2) ** 2)) for g1, g2 in zip(geoms[1:], geoms)]
         max_i = np.argmax(dists)
-        logger.info("Inserting image between %d and %d with Cartesian RMSD %10.3f.  New length:%d",
-                    max_i, max_i + 1, dists[max_i], len(geoms) + 1)
+        logger.info(
+            "Inserting image between %d and %d with Cartesian RMSD %10.3f. New length: %d",
+            max_i, max_i + 1, dists[max_i], len(geoms) + 1
+        )
         insertion = _mid_point(atoms, geoms[max_i], geoms[max_i + 1], tol)
         _, insertion = align_geom(geoms[max_i], insertion)
         geoms.insert(max_i + 1, insertion)
         geoms = list(align_path(geoms)[1])
-    # If there are too many images, remove points
-    while len(geoms) > nimages:
+
+    # Remove points if there are too many images
+    while len(geoms) > n_images:
         dists = [np.sqrt(np.mean((g1 - g2) ** 2)) for g1, g2 in zip(geoms[2:], geoms)]
         min_i = np.argmin(dists)
-        logger.info("Removing image %d.  Cartesian RMSD of merged section %10.3f",
-                    min_i + 1, dists[min_i])
+        logger.info(
+            "Removing image %d. Cartesian RMSD of merged section %10.3f",
+            min_i + 1, dists[min_i]
+        )
         del geoms[min_i + 1]
         geoms = list(align_path(geoms)[1])
+
     return geoms
