45794d72013ebdb54304d25cf881f5f2ea4571c8
Louie Slocombe <31584275+LouieSlocombe@users.noreply.github.com>
2025-11-20 11:51:27 -0700

    Simplify setup.py by removing configuration
    Removed package metadata and dependencies from setup.py.

--- setup.py
+++ setup.py
@@ -1,37 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='geodesic_interpolate',
-    version='0.0.1',
-    author='Louie Slocombe',
-    author_email='louies@hotmail.co.uk',
-    description='Interpolation and smoothing of reaction paths with geodesics in '
-                'redundant internal coordinates.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/LouieSlocombe/geodesic_interpolate',
-    packages=find_packages(include=['geodesic_interpolate', 'geodesic_interpolate.*']),
-    package_data={
-        'geodesic_interpolate': [
-            'data/*',
-        ],
-    },
-    include_package_data=True,
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.12',
-    install_requires=[
-        'numpy>=1.26.4',
-        'scipy>=1.16.0',
-        'ase>=3.25.0',
-    ],
-    extras_require={
-        'dev': [
-            'pytest',
-            'pytest-cov',
-        ],
-    },
-)
+setup()
